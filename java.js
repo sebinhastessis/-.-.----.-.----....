45517a9f03b1925babb8da626b3f5534fff612a0
sebinhastessis <134986110+sebinhastessis@users.noreply.github.com>
2023-12-06 19:32:12 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+var AAAAAAAAAAAAAAAAAAAAAAAAAAAA = ["html","css","java"];
+document.getElementById("ß").innerHTML=AAAAAAAAAAAAAAAAAAAAAAAAAAAA + " " + AAAAAAAAAAAAAAAAAAAAAAAAAAAA [2];
+var eeeeeeeeeeeeeeeeeeeeeeeeeeee = ["minecraft","roblox","GTA"];
+document.getElementById("Ö").innerHTML=eeeeeeeeeeeeeeeeeeeeeeeeeeee + " " + eeeeeeeeeeeeeeeeeeeeeeeeeeee [1];
+var iiiiiiiiiiiiiiiiiiiiiiiiiiii = ["sushi","hamburgir","arroz"];
+document.getElementById("Ü").innerHTML=iiiiiiiiiiiiiiiiiiiiiiiiiiii + " " + iiiiiiiiiiiiiiiiiiiiiiiiiiii [0];
+var oooooooooooooooooooooooooooo = ["verde","turquesa","azul"];
+document.getElementById("Ä").innerHTML=oooooooooooooooooooooooooooo + " " + oooooooooooooooooooooooooooo [1];
